745af8c8111ac2e1c8a27012360348d6f68c5011
Erik Flores <erik.flores@pedidosya.com>
2025-09-05 16:56:18 -0300

    Initial Commit

---
+++ ByeMoney/ByeMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByeMoneyApp.swift
+//  ByeMoney
+//
+//  Created by Erik Flores Quispe on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct ByeMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ByeMoney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ByeMoney
+//
+//  Created by Erik Flores Quispe on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
